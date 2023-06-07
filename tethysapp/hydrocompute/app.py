9f9ea77630eefe9f97b468dbab40f572ff61704c
Nathan Swain <nswain@aquaveo.com>
2023-06-07 16:52:17 -0600

    Rename app and change to UI color

--- tethysapp/hydrocompute/app.py
+++ tethysapp/hydrocompute/app.py
@@ -5,14 +5,15 @@ class Hydrocompute(TethysAppBase):
     """
     Tethys app class for HydroCompute.
     """
-
-    name = 'HydroCompute'
-    description = 'Tethys app that demonstrates the capabilities of the HydroCompute library'
+    name = 'HydroLang and HydroCompute Demonstration'
+    description = 'This application demonstrates the capabilities of HydroLang and HydroCompute, ' \
+                  'powerful JavaScript libraries developed by the University of Iowa ' \
+                  'Hydro- informatics Lab. Launch to learn more.'
     package = 'hydrocompute'  # WARNING: Do not change this value
     index = 'home'
     icon = f'{package}/images/sticker_uihilab2_white.png'
-    root_url = 'hydrocompute'
-    color = '#f39c12'
-    tags = ''
+    root_url = 'uihi'
+    color = '#ffd700'
+    tags = '"Hydrology","Compute","Hydrologic Modeling","JavaScript"'
     enable_feedback = False
     feedback_emails = []
